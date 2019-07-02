1c2a5cb9b0f413392281ea24d88c525915423a39
Hao Wang <ge_nah@hotmail.com>
2019-07-01 22:46:51 -0700

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,4 @@
+
 class SongsController < ApplicationController
   def index
     @songs = Song.all
@@ -19,6 +20,7 @@ class SongsController < ApplicationController
     else
       render :new
     end
+
   end
 
   def edit
@@ -47,7 +49,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, :genre_id,  note_contents: [])
   end
 end
 
